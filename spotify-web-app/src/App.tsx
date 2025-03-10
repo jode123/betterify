3b72e3b56f1b8b3138175d0c7c2012d7595eb6fa
jode123 <50594514+jode123@users.noreply.github.com>
2025-03-10 12:02:17 +0000

    t

--- spotify-web-app/src/App.tsx
+++ spotify-web-app/src/App.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { View, StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-web';
 
-// Fix import paths by removing 'src'
 import Login from './components/Login';
 import Callback from './components/Callback';
 import AuthGuard from './components/AuthGuard';
 import TabNavigator from './navigation/TabNavigator';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle="light-content" />
-      <Router>
-        <View style={{ flex: 1, backgroundColor: '#121212' }}>
-          <Routes>
-            <Route path="/" element={<AuthGuard><TabNavigator /></AuthGuard>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/callback" element={<Callback />} />
-          </Routes>
-        </View>
-      </Router>
-    </SafeAreaProvider>
+    <Router>
+      <SafeAreaView 
+        style={{ 
+          flex: 1, 
+          backgroundColor: '#121212',
+          minHeight: '100vh',
+          width: '100%'
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<AuthGuard><TabNavigator /></AuthGuard>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/callback" element={<Callback />} />
+        </Routes>
+      </SafeAreaView>
+    </Router>
   );
 };
 
--- spotify-web-app/src/types/react-native-web.d.ts
+++ spotify-web-app/src/types/react-native-web.d.ts
@@ -1,7 +1,3 @@
 declare module 'react-native-web' {
   export * from 'react-native';
 }
-
-declare module '@expo/vector-icons' {
-  export * from '@expo/vector-icons/build/IconTypes';
-}
